3780f19b74d9ccd37c4be6bb5dd4a9488801d046
tsmat682 <43554812+tsmat682@users.noreply.github.com>
2019-08-08 13:27:21 -0700

    Add files via upload

---
+++ Hello_World/FirstClass.java
@@ -0,0 +1,32 @@
+
+/**
+ * Write a description of class FirstClass here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class FirstClass
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class FirstClass
+     */
+    public FirstClass()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here i want some lard
+        return x + y;
+    }
+}
